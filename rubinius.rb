b0fd295b402f1b6394838785e6ef9486267bb1c5
Matijs van Zuijlen <matijs@matijs.net>
2016-02-12 22:09:41 +0100

    Use ObjectSpace.each_object to find all call sites

--- rubinius.rb
+++ rubinius.rb
@@ -46,34 +46,21 @@ main
 
 m = method :main
 
-def recurse_call_sites(x, count)
-  return if count > 7
-  x.call_sites.each do |call_site|
-    printable = call_site.location =~ /tracy/
-    printable = true
-    case call_site
-    when Rubinius::PolyInlineCache
-      puts "[#{count}] PolyInlineCache at #{call_site.location}" if printable
-      call_site.entries.each do |entry|
-        next unless entry
-        puts "[#{count}] Cache entry for method #{entry.receiver_class}::#{call_site.name} at #{call_site.location}" if printable
-        begin
-          recurse_call_sites(entry.method, count + 1)
-        rescue => e
-          require 'pry'
-          binding.pry
-        end
-      end
-    when Rubinius::MonoInlineCache
-      puts "[#{count}] Mono cache for method #{call_site.receiver_class}::#{call_site.name} at #{call_site.location}" if printable
-      recurse_call_sites(call_site.method, count + 1)
-    when Rubinius::CallSite
-      puts "[#{count}] Plain compiled_code inside #{x.name}; #{call_site.executable == x}" if printable
-    else
-      p call_site if printable
-      recurse_call_sites(call_site.method, count + 1)
+call_sites = ObjectSpace.each_object(Rubinius::CallSite).map(&:itself)
+
+call_sites.each do |call_site|
+  next if call_site.is_a? Rubinius::InlineCacheEntry
+  next unless call_site.location =~ /tracy/
+  case call_site
+  when Rubinius::PolyInlineCache
+    puts "PolyInlineCache at #{call_site.location}"
+    call_site.entries.each do |entry|
+      next unless entry
+      puts "  Cache entry for method #{entry.receiver_class}::#{call_site.name} at #{call_site.location}"
     end
+  when Rubinius::MonoInlineCache
+    puts "Mono cache for method #{call_site.receiver_class}::#{call_site.name} at #{call_site.location}"
+  when Rubinius::CallSite
+    puts "CallSite #{call_site.name} inside #{call_site.executable.name} at #{call_site.location}"
   end
 end
-
-recurse_call_sites(m.executable, 0)
