9c6a8b6420f6f8fa1cbcc25aefc0a8787800b2ff
Matijs van Zuijlen <matijs@matijs.net>
2016-02-13 16:53:06 +0100

    Extract printing code into methods

--- rubinius.rb
+++ rubinius.rb
@@ -48,40 +48,46 @@ m = method :__script__
 
 EXECUTABLES = {}
 
-def recurse_call_sites(executable, count)
+def print_executable(executable, count)
   return unless executable.is_a? Rubinius::CompiledCode
   return if EXECUTABLES[executable]
   EXECUTABLES[executable] = 1
 
-  indent = '   ' * count
-
   printable = executable.file =~ /tracy/
+  indent = '    ' * count
 
   puts "#{indent}Executable #{executable.name} in #{executable.file} line #{executable.defined_line}" if printable
 
   executable.call_sites.each do |call_site|
-    case call_site
-    when Rubinius::PolyInlineCache
-      puts "#{indent} PolyInlineCache at #{call_site.location}" if printable
-      call_site.entries.each do |entry|
-        next unless entry
-        puts "#{indent}  Cache entry for method #{entry.receiver_class}::#{call_site.name} at #{call_site.location}" if printable
-        recurse_call_sites(entry.method, count + 1)
-      end
-    when Rubinius::MonoInlineCache
-      puts "#{indent} Mono cache for method #{call_site.receiver_class}::#{call_site.name} at #{call_site.location}" if printable
-      recurse_call_sites(call_site.method, count + 1)
-    when Rubinius::CallSite
-      puts "#{indent} CallSite #{call_site.name} at #{call_site.location}" if printable
-    else
-      p call_site
-      recurse_call_sites(call_site.method, count + 1)
-    end
+    print_call_site(call_site, count + 1, printable)
   end
 
   executable.child_methods.each do |ex|
-    recurse_call_sites(ex, count + 1)
+    print_executable(ex, count + 1)
   end
 end
 
-recurse_call_sites(m.executable, 0)
+def print_call_site(call_site, count, printable)
+  indent = '    ' * count
+
+  case call_site
+  when Rubinius::PolyInlineCache
+    puts "#{indent}PolyInlineCache at #{call_site.location}" if printable
+    call_site.entries.each do |entry|
+      next unless entry
+      print_cache_entry(call_site, entry, count + 1, printable)
+    end
+  when Rubinius::MonoInlineCache
+    puts "#{indent}MonoInlineCache at #{call_site.location} calling #{call_site.receiver_class}::#{call_site.name}" if printable
+    print_executable(call_site.method, count + 1)
+  when Rubinius::CallSite
+    puts "#{indent}CallSite #{call_site.name} at #{call_site.location}" if printable
+  end
+end
+
+def print_cache_entry(call_site, entry, count, printable)
+  indent = '    ' * count
+  puts "#{indent}Cache entry for method #{entry.receiver_class}::#{call_site.name} at #{call_site.location}" if printable
+  print_executable(entry.method, count + 1)
+end
+print_executable(m.executable, 0)
