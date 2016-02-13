4660aef93049c55b29e7d8e974684cc715640e90
Matijs van Zuijlen <matijs@matijs.net>
2016-02-13 18:02:12 +0100

    Clean up

--- rubinius.rb
+++ rubinius.rb
@@ -48,26 +48,26 @@ m = method :__script__
 
 EXECUTABLES = {}
 
-def print_executable(executable, count)
+def print_executable(executable)
   return unless executable.is_a? Rubinius::CompiledCode
   return if EXECUTABLES[executable]
   EXECUTABLES[executable] = 1
 
   executable.call_sites.each do |call_site|
-    print_call_site(call_site, count + 1)
+    print_call_site(call_site)
   end
 
   executable.child_methods.each do |ex|
-    print_executable(ex, count + 1)
+    print_executable(ex)
   end
 end
 
-def print_call_site(call_site, count)
+def print_call_site(call_site)
   case call_site
   when Rubinius::PolyInlineCache
     call_site.entries.each do |entry|
       next unless entry
-      print_cache_entry(call_site, entry, count + 1)
+      print_cache_entry(call_site, entry)
     end
   when Rubinius::MonoInlineCache
     callee = call_site.method
@@ -80,12 +80,12 @@ def print_call_site(call_site, count)
       callee_scope = callee.scope
       p [callee.name, callee_scope ? callee_scope.module : '', callee.defined_line, callee.file]
     end
-    print_executable(callee, count + 1)
+    print_executable(callee)
   when Rubinius::CallSite
   end
 end
 
-def print_cache_entry(call_site, entry, count)
+def print_cache_entry(call_site, entry)
   callee = entry.method
   if callee.is_a?(Rubinius::CompiledCode) && callee.file =~ /tracy/
     executable = call_site.executable
@@ -96,7 +96,7 @@ def print_cache_entry(call_site, entry, count)
     callee_scope = callee.scope
     p [callee.name, callee_scope ? callee_scope.module : '', callee.defined_line, callee.file]
   end
-  print_executable(callee, count + 1)
+  print_executable(callee)
 end
 
-print_executable(m.executable, 0)
+print_executable(m.executable)
