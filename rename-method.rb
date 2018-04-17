d78b3f86b77eb1ae8308fa73cec7f9489c583f8e
Matijs van Zuijlen <matijs@matijs.net>
2018-04-17 23:24:33 +0200

    Autocorrect Style/Next

--- rename-method.rb
+++ rename-method.rb
@@ -23,15 +23,14 @@ filenames = rename_locations.map {|caller| caller[1]}.sort.uniq
 filenames.each do |calling_name|
   lines = File.readlines(calling_name)
   rename_locations.each do |loc_line, loc_file|
-    if calling_name == loc_file
-      unless lines[loc_line - 1] =~ /\b#{old_name}\b/
-        raise "Method #{old_name} not found on line #{loc_line} in #{calling_name}"
-      end
-      if lines[loc_line - 1] =~ /\b#{old_name}\b.*\b#{old_name}\b/
-        raise "More than one match at line #{loc_line} in #{calling_name}"
-      end
-      lines[loc_line - 1].gsub!(/\b#{old_name}\b/, new_name)
+    next unless calling_name == loc_file
+    unless lines[loc_line - 1] =~ /\b#{old_name}\b/
+      raise "Method #{old_name} not found on line #{loc_line} in #{calling_name}"
     end
+    if lines[loc_line - 1] =~ /\b#{old_name}\b.*\b#{old_name}\b/
+      raise "More than one match at line #{loc_line} in #{calling_name}"
+    end
+    lines[loc_line - 1].gsub!(/\b#{old_name}\b/, new_name)
   end
   puts "# --- #{calling_name} ---"
   lines.each do |line|
